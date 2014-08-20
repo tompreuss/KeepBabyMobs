997081d25652a58938ec6a15fda4376be42a3b73
tompreuss <tompreuss@gmail.com>
2014-08-19 22:20:57 -0400

    add logging of killing of age locked mobs

--- src/main/java/io/github/tompreuss/keepbabymobs/KeepBabyMobs.java
+++ src/main/java/io/github/tompreuss/keepbabymobs/KeepBabyMobs.java
@@ -6,10 +6,14 @@ import org.bukkit.Material;
 import org.bukkit.entity.Ageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Horse;
+import org.bukkit.entity.Ocelot;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Tameable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -49,11 +53,11 @@ public final class KeepBabyMobs extends JavaPlugin implements Listener {
                                             String name = meta.getDisplayName();
                                                                                                                     
                                             String locationlog = location.getWorld().getName() + " "
-                                            + location.getBlockX() + " " + location.getBlockY() + " "
-                                                    + location.getBlockZ();
+                                                + location.getBlockX() + " " + location.getBlockY() + " "
+                                                + location.getBlockZ();
                                             
                                             getLogger().info(playername + " age locked " + entitytype + " named " 
-                                                    + name + " at "  + locationlog );
+                                                + name + " at "  + locationlog );
                                         }
                                     }
                                 }
@@ -62,5 +66,53 @@ public final class KeepBabyMobs extends JavaPlugin implements Listener {
             }
         
     } // onPlayerInteractEntityEvent
+    
+    // if an age locked baby mob is killed, log it
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onEntityDeathEvent(EntityDeathEvent event) {
+        Entity entity = event.getEntity();
+        Player player = event.getEntity().getKiller();
+            if (player != null && entity != null) { // player killing isn't null and entity killed isn't null
+                if (entity instanceof Ageable) { // entity is one that has babies
+                    if (((Ageable) entity).getAgeLock() == true) { // if entity is age locked
+                        
+                        // log the killing of the mob
+                        
+                        EntityType entitytype = entity.getType();
+                        Location location = entity.getLocation();
+                        String playername = player.getName();
+                        String name = ((Ageable) entity).getCustomName();
+                        
+                        String locationlog = location.getWorld().getName() + " " + location.getBlockX() + " "
+                            + location.getBlockY() + " " + location.getBlockZ();
+                        
+                        StringBuilder logmessage = new StringBuilder();
+                        logmessage.append(playername + " killed an age locked " + entitytype);
+
+                        if (entity instanceof Tameable) { // if tameable  
+                            Tameable tameable = (Tameable) entity;
+                            if (tameable instanceof Ocelot) { // if cat
+                                Ocelot ocelot = (Ocelot) tameable;
+                                logmessage.append(" of type " + ocelot.getCatType().name());
+                            } else if (tameable instanceof Horse) { // if horse
+                                Horse horse = (Horse) tameable;
+                                logmessage.append(" of type " + horse.getVariant().name() + " "
+                                + horse.getColor().name() + " " + horse.getStyle().name());
+                            }
+                            
+                            if (tameable.isTamed()) { // if has owner
+                                logmessage.append(" owned by " + tameable.getOwner().getName());
+                            }
+                            
+                        }
+                        
+                        logmessage.append(" named " + name + " at "  + locationlog);
+                        
+                        getLogger().info(logmessage.toString());
+                        
+                    }
+                }
+            } 
+    } //onEntityDeathEvent
     
 } // class KeepBabyMobs
